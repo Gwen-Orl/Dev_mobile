f168b6d50add990f71984b5b09a712fdcc00d953
Gwen-Orl <gwendan.pupp-baudson@etu.univ-orleans.fr>
2026-03-09 09:29:55 +0100

    fin de td

--- projet2/lib/UI/ecran2.dart
+++ projet2/lib/UI/ecran2.dart
@@ -5,72 +5,47 @@ import '../api/myapi.dart';
 
 class ecran2 extends StatelessWidget {
   ecran2({super.key});
+
   @override
   Widget build(BuildContext context) {
-    Future<List<Task>> tasks=MyAPI().getTasks();
-    //print(tasks.length);
-    return ListView.builder(
-      itemBuilder : (BuildContext context ,int index) {
-        return Card(
-            color: Colors.black,
-            elevation: 7,
-            margin: const EdgeInsets.all(10),
-            child:
-            FutureBuilder <List<Task>>(
-                future: tasks,
-                builder: (BuildContext context, AsyncSnapshot<List<Task>> snapshot) {
-                  List<Widget> child;
-                  if (snapshot.hasData) {
-                    child = <Widget>[
-                      Card(
-                        color: Colors.black,
-                        elevation: 7,
-                        margin: const EdgeInsets.all(10),
-                        child:ListTile(
-                          leading: CircleAvatar(backgroundColor: Colors.green, child:Text(snapshot.data?[index].id.toString()??""),),
-                          title: Text(snapshot.data?[index].title??""),
-                          subtitle: Text(snapshot.data?[index].tags.join(" ")??""),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () {
-                            },
-                          ),
-                        ),
-                      )
-                    ];
-                  } else if (snapshot.hasError) {
-                    child = <Widget>[
-                      const Icon(Icons.error_outline, color: Colors.red, size: 60),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16),
-                        child: Text('Error: ${snapshot.error}'),
-                      ),
-                    ];
-                  } else {
-                    child = const <Widget>[
-                      SizedBox(
-                        width: 60,
-                        height: 60,
-                        child: CircularProgressIndicator(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 16),
-                        child: Text('Awaiting result...'),
-                      ),
-                    ];
-                  }
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: child,
+    return FutureBuilder(
+        future: MyAPI().getTasks(),
+        builder: (context, snapshot){
+          if (snapshot.connectionState!=ConnectionState.done && !snapshot.hasData){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError){
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          if (snapshot.data != null){
+            return ListView.builder(
+              itemCount: snapshot.data?.length??0,
+              itemBuilder: (BuildContext context, index){
+                return Card(
+                  color: Colors.white,
+                  elevation: 7,
+                  margin: const EdgeInsets.all(10),
+                  child :
+                  ListTile(
+                    leading: CircleAvatar(backgroundColor: Colors.green, child:Text(snapshot.data?[index].id.toString()??""),),
+                    title: Text(snapshot.data?[index].title??""),
+                    subtitle: Text(snapshot.data?[index].tags.join(" ")??""),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                      },
                     ),
+                  ),
+                );
+              },
+            );
+          }
+          return Container();
+        }
 
-                  );
-                }
-            )
-        );
-      },
     );
-
   }
 }
--- projet2/lib/UI/ecran3.dart
+++ projet2/lib/UI/ecran3.dart
@@ -1,28 +1,52 @@
 import 'package:flutter/material.dart';
+import '../modele/task.dart';
+import 'package:flutter/material.dart';
+import '../api/myapi.dart';
+import 'package:http/http.dart' as http;
 
 class ecran3 extends StatelessWidget {
-  const ecran3({super.key});
+  ecran3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Ecran Foucher'),
-          centerTitle: true,
-          backgroundColor: Colors.purple,
-        ),
-        backgroundColor: Colors.brown,
-        body: NotificationListener(
-            child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Text("Villerable"),
-                  ],
-                )
-            )
-        )
+    return FutureBuilder(
+        future: MyAPI().getTasks(),
+        builder: (context, snapshot){
+          if (snapshot.connectionState!=ConnectionState.done && !snapshot.hasData){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError){
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          if (snapshot.data != null){
+            return ListView.builder(
+              itemCount: snapshot.data?.length??0,
+              itemBuilder: (BuildContext context, index){
+                return Card(
+                  color: Colors.white,
+                  elevation: 7,
+                  margin: const EdgeInsets.all(10),
+                  child :
+                  ListTile(
+                    leading: CircleAvatar(backgroundColor: Colors.green, child:Text(snapshot.data?[index].id.toString()??""),),
+                    title: Text(snapshot.data?[index].title??""),
+                    subtitle: Text(snapshot.data?[index].tags.join(" ")??""),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                      },
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+          return Container();
+        }
+
     );
   }
 }
-
